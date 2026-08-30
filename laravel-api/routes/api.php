bb9a9a01d06432e213ac7ee3fd6fb83828c9b738
Jamal Abu Attaya <jamalataya3@gmail.com>
2026-08-30 12:30:59 +0300

    feat: add protected course learning API

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -14,18 +14,39 @@ Route::prefix('v1')->middleware('throttle:api')->group(function (): void {
 
     Route::get('/courses', [CourseController::class, 'index']);
     Route::get('/courses/{course:slug}', [CourseController::class, 'show']);
+
     Route::prefix('catalog')->group(function (): void {
         Route::get('/books', [CatalogController::class, 'books']);
         Route::get('/courses', [CatalogController::class, 'courses']);
         Route::get('/articles', [CatalogController::class, 'articles']);
-        Route::get('/articles/{article}', [CatalogController::class, 'article'])->whereNumber('article');
+        Route::get('/articles/{article}', [CatalogController::class, 'article'])
+            ->whereNumber('article');
     });
 
     Route::middleware('auth:sanctum')->group(function (): void {
         Route::get('/me', [AuthController::class, 'me']);
         Route::post('/auth/logout', [AuthController::class, 'logout']);
+
         Route::get('/dashboard', [LearningController::class, 'dashboard']);
-        Route::post('/courses/{course}/enroll', [LearningController::class, 'enroll']);
-        Route::put('/lessons/{lesson}/progress', [LearningController::class, 'updateProgress']);
+
+        Route::post(
+            '/courses/{course}/enroll',
+            [LearningController::class, 'enroll'],
+        );
+
+        Route::get(
+            '/courses/{course:slug}/learning',
+            [LearningController::class, 'course'],
+        );
+
+        Route::get(
+            '/lessons/{lesson}',
+            [LearningController::class, 'lesson'],
+        );
+
+        Route::put(
+            '/lessons/{lesson}/progress',
+            [LearningController::class, 'updateProgress'],
+        );
     });
 });
